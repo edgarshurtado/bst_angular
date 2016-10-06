e036f17439e3f1313f7b42f7784e078a2d78a644
Edgar S. Hurtado <edsanhu@gmail.com>
2016-10-06 20:05:22 +0200

    Add test text for each view

--- app.js
+++ app.js
@@ -7,9 +7,17 @@ blackShadder.config(function($routeProvider){
         controller: 'mainController'
     })
     .when('/estudio', {
-        templateUrl: 'pages/estudio.html',
+        templateUrl: 'pages/main.html',
         controller: 'estudioController'
-    });
+    })
+    .when('/galeria', {
+        templateUrl: 'pages/main.html',
+        controller: 'galeriaController'
+    })
+    .when('/contacto', {
+        templateUrl: 'pages/main.html',
+        controller: 'contactoController'
+    })
 })
 
 blackShadder.controller("mainController", ["$scope", function($scope){
@@ -18,4 +26,12 @@ blackShadder.controller("mainController", ["$scope", function($scope){
 
 blackShadder.controller("estudioController", ["$scope", function($scope){
     $scope.text = "Example text in estudio";
-}])
+}]);
+
+blackShadder.controller("galeriaController", ["$scope", function($scope){
+    $scope.text = "Example text in galeria";
+}]);
+
+blackShadder.controller("contactoController", ["$scope", function($scope){
+    $scope.text = "Example text in contacto";
+}]);
