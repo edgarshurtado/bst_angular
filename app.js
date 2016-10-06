ba68aec52f85c0f9632094b41117ad9e6f2317d8
Edgar S. Hurtado <edsanhu@gmail.com>
2016-10-06 19:51:20 +0200

    Add main and estudio pages

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-var blackShadder = angular.module("blackShadderWeb", []);
+var blackShadder = angular.module("blackShadderWeb", ['ngRoute']);
+
+blackShadder.config(function($routeProvider){
+    $routeProvider
+    .when('/', {
+        templateUrl: 'pages/main.html',
+        controller: 'testController'
+    })
+    .when('/estudio', {
+        templateUrl: 'pages/estudio.html',
+        controller: 'testController'
+    });
+})
 
 blackShadder.controller("testController", ["$scope", function($scope){
     $scope.text = "Example text";
