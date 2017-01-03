acada0f9852914f159f95c4a66d1046bf21effdd
Edgar S. Hurtado <edsanhu@gmail.com>
2017-01-03 22:56:36 +0100

    Install and implement angularSlideables
    It's an angular directive created by an github contributor that allows
    without css to emulate the behaviour of jQuery `slideDown()` `slideUp()`
    
    credits are in the first line of the imported file.
    
     Changes to be committed:
    	new file:   directives/angularSlideables.js
    	modified:   index.html
    	modified:   pages/main.html
    	new file:   services.js

---
+++ services.js
@@ -0,0 +1,76 @@
+blackShadder.service('ScheduleService', function(){
+    var self = this;
+
+    var scheduleList = {
+        0:{
+            morning: {
+                opening: "9:00",
+                close:"14:00"
+            },
+            afternoon: {
+                opening: "16:00",
+                close: "20:00"
+            }
+        },
+        1:{
+            morning: {
+                opening: "9:00",
+                close:"14:00"
+            },
+            afternoon: {
+                opening: "16:00",
+                close: "20:00"
+            }
+        },
+        2:{
+            morning: {
+                opening: "9:00",
+                close:"14:00"
+            },
+            afternoon: {
+                opening: "16:00",
+                close: "20:00"
+            }
+        },
+        3:{
+            morning: {
+                opening: "9:00",
+                close:"14:00"
+            },
+            afternoon: {
+                opening: "16:00",
+                close: "20:00"
+            }
+        },
+        4:{
+            morning: {
+                opening: "9:00",
+                close:"14:00"
+            },
+            afternoon: {
+                opening: "16:00",
+                close: "20:00"
+            }
+        },
+        5:{
+            morning: {
+                opening: "9:00",
+                close:"14:00"
+            },
+            afternoon: {
+                opening: "16:00",
+                close: "20:00"
+            }
+        },
+        6:{
+            morning: {
+                opening: "9:00",
+                close:"14:00"
+            },
+            afternoon: {
+                opening: "16:00",
+                close: "20:00"
+            }
+        },
+    };
+})
