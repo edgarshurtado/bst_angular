06a1a4939850e9b28cabf923c15a1834c128a768
Edgar S. Hurtado <edsanhu@gmail.com>
2017-05-20 16:53:33 +0200

    Modify photo holders for being lighter
    Delete from repo the images too.

--- controllers/GaleriaController.js
+++ controllers/GaleriaController.js
@@ -2,16 +2,18 @@ blackShadder.controller("galeriaController", ["$scope", "$interval", function($s
 
     // TODO: Have a backend to serve the images
     $scope.photos = [
-        "leon.jpg",
-        "marilyn.jpg",
-        "reloj_rosa.jpg",
-        "foto_tattoorealista.jpg",
-        "egipcio_esfinge.jpg",
-        "gladiador.jpg",
-        "mapa_brujula.jpg",
-        "fenix.jpg",
-        "catrina.jpg",
-        "delfines.jpg"
+        "leon_ivan_morant",
+        "caraninyo",
+        "durotan_ivan_morant",
+        "harley_ivan_morant",
+        "retrato2_ivan_morant",
+        "eduardo_manos_tijeras_ivan_morant",
+        "lobo_ivan_morant",
+        "catrina_ivan_morant"
+        // "brujula_reloj_mapa_ivan_morant.jpg",
+        // "mascara_gas_ivan_morant.jpg",
+        // "pierna_300_ivan_morant.jpg",
+        // "samurai_dragon_ivan_morant.jpg"
     ];
 
     $scope.selected = null;
